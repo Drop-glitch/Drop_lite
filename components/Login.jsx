7371ac578a449250522422bdf3678f5326717c13
Drop-glitch <jqmarques70@gmail.com>
2026-04-24 13:14:33 -0300

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,1 +1,11 @@
+export default function Login({ onLogin }) {
+  return (
+    <main className="container">
+      <h1>Drops Lite</h1>
+      <p>Conecte-se com quem está por perto</p>
 
+      <input placeholder="Seu email" />
+      <button onClick={onLogin}>Enviar código</button>
+    </main>
+  )
+}
