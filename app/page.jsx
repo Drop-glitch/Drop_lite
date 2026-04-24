06843b3a84898caa7709ab01a0b830e2348a3918
Drop-glitch <jqmarques70@gmail.com>
2026-04-24 12:35:42 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,25 @@
 export default function Page() {
-  return <h1>Drops Lite</h1>
+  const users = [
+    { nome: "Lucas", km: "0,4 km" },
+    { nome: "Marina", km: "0,8 km" },
+    { nome: "Rafa", km: "1,2 km" },
+    { nome: "Bianca", km: "1,9 km" },
+  ]
+
+  return (
+    <main className="container">
+      <h1>Drops Lite</h1>
+      <p className="sub">Pessoas perto de você</p>
+
+      {users.map((user, i) => (
+        <div className="card" key={i}>
+          <div>
+            <strong>{user.nome}</strong>
+            <p>{user.km}</p>
+          </div>
+          <button>Ver perfil</button>
+        </div>
+      ))}
+    </main>
+  )
 }
