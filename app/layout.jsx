cf1265f046beab21182434bb617889aaef23983f
Drop-glitch <jqmarques70@gmail.com>
2026-04-24 12:34:42 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,9 @@
-pexport default function RootLayout({ children }) {
+export const metadata = {
+  title: "Drops Lite",
+  description: "Rede social local",
+}
+
+export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
