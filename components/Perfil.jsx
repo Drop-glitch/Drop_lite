5571f4e77bfc3bd8f7dffd2b244a5dd3642ac93c
Drop-glitch <jqmarques70@gmail.com>
2026-04-24 13:15:11 -0300

    Update Perfil.jsx

--- components/Perfil.jsx
+++ components/Perfil.jsx
@@ -1,1 +1,17 @@
+export default function Perfil() {
+  return (
+    <section className="card">
+      <img
+        src="https://via.placeholder.com/90"
+        className="avatar"
+      />
 
+      <h2>Lucas Mendes</h2>
+      <p>@lucasm</p>
+      <p>Curto música e parques 🌅</p>
+
+      <button>Editar perfil</button>
+      <button>Postar Drop</button>
+    </section>
+  )
+}
